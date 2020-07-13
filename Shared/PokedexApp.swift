2111367a13dccf85aa4e2f5cdbc2b0872c9f232c
clearchaos3 <clearchaos3@gmail.com>
2020-07-13 13:38:33 -0500

    Initial Commit

---
+++ Shared/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Shared
+//
+//  Created by Ryan Lee on 7/13/20.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
